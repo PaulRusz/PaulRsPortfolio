66c6bcab0d42ddd6f99783fe6db85acc48796552
PaulRusz <paulruszkay@gmail.com>
2024-06-06 19:36:08 -0400

    added Resume

--- src/componets/Resume.jsx
+++ src/componets/Resume.jsx
@@ -3,6 +3,21 @@ import '../styles/Resume.css';
 
 function Resume() {
 
+    return (
+        <div className="resume-container">
+            <h1>Resume</h1>
+            <div className="resume-content">
+                <h2>Download my resume</h2>
+                <a href="https://drive.google.com/file/d/1B4pP-tSPWAWygiHZH3iD19f2UidwRtTx/view?usp=drive_link"
+                 download="Resume.pdf">Click here to download my resume</a>
+                <h2>Summary</h2>
+                <p>
+                    Background:
+                    - what Ive done and what I am good at tech wise? Profeciencies
+                </p>
+            </div>
+        </div>
+    )
 }
 
 export default Resume;
