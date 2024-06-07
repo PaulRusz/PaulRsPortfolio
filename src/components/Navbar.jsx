3d0c5fc42ea2358ab7c5aa2f02fddbc9a17b955f
PaulRusz <paulruszkay@gmail.com>
2024-06-07 07:26:23 -0400

    navbar adjustments

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,10 +7,12 @@ function Navbar() {
     return (
 
         <nav className="navbar">
+            
             <Link to="/" className="links">About Me</Link>
             <Link to="/portfolio" className="links">Portfolio</Link>
             <Link to="/contact" className="links">Contact</Link>
             <Link to="/resume" className="links">Resume</Link>
+            
         </nav>
 
     )
