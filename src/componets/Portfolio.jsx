0dd5366a0fa2348f5ecfcbc37772aa86caa32e41
PaulRusz <paulruszkay@gmail.com>
2024-06-06 20:37:59 -0400

    added portfolio jsx & css

--- src/componets/Portfolio.jsx
+++ src/componets/Portfolio.jsx
@@ -3,6 +3,18 @@ import '../styles/Portfolio.css';
 
 function Portfolio() {
 
+    return (
+        <div className="portfolio">
+            <h1>Portfolio</h1>
+            <p>Here are some of my projects:</p>
+            <ul>
+                    <a href="https://github.com/username/project1" target="_blank" rel=""> Project 1 </a>
+            </ul>
+            <ul>
+                    <a href="https://github.com/username/project2" target="_blank" rel=""> Project 2</a>
+            </ul>
+        </div>
+    )
 }
 
 export default Portfolio;
