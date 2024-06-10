70ba3bc1e7ab8698c54fa1fe7c020638d94a2843
PaulRusz <paulruszkay@gmail.com>
2024-06-10 14:55:21 -0400

    changed css to help with mobile settings

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,11 @@
 // Imports the Navbar.css to grant access to additional classNames
+import { useState } from 'react';
 import '../styles/Navbar.css';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
+
+    const [showMenu, setShowMenu ] = useState(false)
 
     return (
 
@@ -10,12 +13,18 @@ function Navbar() {
             <div>
                 <p className="logo"> PR </p>
             </div>
+
+            <button className="hamburger-menu" onClick={() => setShowMenu(!showMenu)}>
+                &#9776;
+                </button>
             
             
+            <div className={`links ${showMenu ? 'show' : ''}`}>
             <Link to="/" className="links">About Me</Link>
             <Link to="/portfolio" className="links">Portfolio</Link>
             <Link to="/contact" className="links">Contact</Link>
             <Link to="/resume" className="links">Resume</Link>
+            </div>
             
             
         </nav>
