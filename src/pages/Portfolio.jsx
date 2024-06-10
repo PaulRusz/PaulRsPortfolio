61f45bde465536406989f7dc6156634813dda1c2
PaulRusz <paulruszkay@gmail.com>
2024-06-10 12:26:22 -0400

    minor changes

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -18,10 +18,22 @@ function Portfolio() {
             <h1>Portfolio</h1>
             <p>Here are some of my projects:</p>
             <ul>
-                    <a href="https://github.com/username/project1" target="_blank" rel=""> Project 1 </a>
+                    <a href="https://github.com/PaulRusz/Personal-Note-Taker" target="_blank" rel=""> Personal Note App </a>
             </ul>
             <ul>
-                    <a href="https://github.com/username/project2" target="_blank" rel=""> Project 2</a>
+                    <a href="https://github.com/PaulRusz/Group-Project1" target="_blank" rel=""> Workout Tracker</a>
+            </ul>
+            <ul>
+                    <a href="https://workout-tracker-3b0i.onrender.com/" target="_blank" rel=""> Workout Tracker 2.0</a>
+            </ul>
+            <ul>
+                    <a href="https://github.com/PaulRusz/project2" target="_blank" rel=""> Project 4</a>
+            </ul>
+            <ul>
+                    <a href="https://github.com/PaulRusz/project2" target="_blank" rel=""> Project 5</a>
+            </ul>
+            <ul>
+                    <a href="https://github.com/PaulRusz/project2" target="_blank" rel=""> Project 6</a>
             </ul>
         </div>
 
