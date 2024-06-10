c6678fbb9423eeccc65d03a9214ffc564d219fa2
PaulRusz <paulruszkay@gmail.com>
2024-06-10 13:26:00 -0400

    changes to about me

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -2,6 +2,8 @@
 import '../styles/AboutMe.css';
 import { motion } from "framer-motion";
 
+import monacoImage from "src/assets/Monaco.png"; 
+
 function AboutMe() {
 
     return (
@@ -15,19 +17,16 @@ function AboutMe() {
       >
 
         <div className="about-me-container">
-            <h1>             <img className="monacoImage" src="src/assets/Monaco.png" alt="Me in Monaco" />About Me             <img className="weddingImage" src="src/assets/wedding.png" alt="Wedding Image" /></h1>
+            <h1>             <img src={monacoImage}  className="monacoImage"  alt="Me in Monaco" />About Me             <img className="weddingImage" src="src/assets/wedding.png" alt="Wedding Image" /></h1>
 
-            <p> 
-            Hey! My name is Paul. <br></br> I’m from New Jersey originally but have lived in Delaware a large majority of my life.  
-
-            I attended college in Newark, Delaware and Nashville, Tennessee where I earned an undergrad degree in business.
-        
-            I’m fairly new to coding and prior to the Columbia University Coding Bootcamp, 
-            I was only self taught using websites and phone applications to learn how to code.
-            
-            While I’m sure I have a long way to go before I can consider myself efficient with coding, 
-            I’m eager to learn and passionate about continuing to create and solve complex problems.
-            </p>
+            <p>Hey there! I'm Paul, a New Jersey native who has called Delaware home for most of my life.</p>
+      <p>My educational journey took me to Newark, Delaware, and Nashville, Tennessee, where I earned a bachelor's degree in business. However, my passion for technology led me down a different path.</p>
+      <p>In the past year, I embarked on a self-taught coding journey, delving into the intricacies of programming. This newfound interest inspired me to enroll in a full stack development course at Columbia University.</p>
+      <p>While I acknowledge that there's always more to learn, I thrive on the challenge of tackling complex problems and honing my coding skills. I'm excited about the possibilities that lie ahead as I continue to grow in this dynamic field.</p>
+      <p>When I'm not immersed in work or coding projects, you can find me staying active through workouts, exploring new destinations to experience the diverse cultures (and of course food), and soaking up sun outdoors.</p>
+      <p>I also spend my time diving into captivating reads, looking for newly released music, and always searching for the best lasagna the East Coast has to offer.</p>
+      <p>A few of my favorites books include the Game of Thrones series, The Poppy War series, and On Earth We’re Briefly Gorgeous.</p>
+      <p>While there's much more to me than what's listed here, don't hesitate to reach out if you'd like to learn more.</p>
         </div>
 
         </motion.div>
