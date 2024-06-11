ea2e089ab2cbae8d93826ac4a6fecd69ea83035b
PaulRusz <paulruszkay@gmail.com>
2024-06-11 14:12:30 -0400

    attempted to change navbar for mobile but didn't work

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,37 @@
 // Imports the Navbar.css to grant access to additional classNames
-import { useState } from 'react';
-import '../styles/Navbar.css';
-import { Link } from 'react-router-dom';
+import { useState } from "react";
+import "../styles/Navbar.css";
+import { Link } from "react-router-dom";
 
 function Navbar() {
+  const [showMenu, setShowMenu] = useState(false);
 
-    const [showMenu, setShowMenu ] = useState(false)
+  return (
+    <nav className="navbar">
+      <div>
+        <p className="logo"> PR </p>
+      </div>
 
-    return (
+      <button className="hamburger-menu" onClick={() => setShowMenu(!showMenu)}>
+        &#9776;
+      </button>
 
-        <nav className="navbar">
-            <div>
-                <p className="logo"> PR </p>
-            </div>
-
-            <button className="hamburger-menu" onClick={() => setShowMenu(!showMenu)}>
-                &#9776;
-                </button>
-            
-            
-            <div className={`links ${showMenu ? 'show' : ''}`}>
-            <Link to="/" className="links">About Me</Link>
-            <Link to="/portfolio" className="links">Portfolio</Link>
-            <Link to="/contact" className="links">Contact</Link>
-            <Link to="/resume" className="links">Resume</Link>
-            </div>
-            
-            
-        </nav>
-
-    )
+      <div className={`links ${showMenu ? "show" : ""}`}>
+        <Link to="/" className="links">
+          About Me
+        </Link>
+        <Link to="/portfolio" className="links">
+          Portfolio
+        </Link>
+        <Link to="/contact" className="links">
+          Contact
+        </Link>
+        <Link to="/resume" className="links">
+          Resume
+        </Link>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
